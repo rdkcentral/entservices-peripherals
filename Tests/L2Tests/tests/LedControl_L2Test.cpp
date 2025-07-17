df4757a5347804720d7fb2ec07816aefd7ed9730
balav08 <54432605+balav08@users.noreply.github.com>
2025-07-17 15:57:09 +0530

    Update LedControl_L2Test.cpp

--- Tests/L2Tests/tests/LedControl_L2Test.cpp
+++ Tests/L2Tests/tests/LedControl_L2Test.cpp
@@ -103,8 +103,8 @@ LEDControl_L2test::~LEDControl_L2test()
 
     /* Deactivate plugin in destructor */
     status = DeactivateService("org.rdk.LEDControl");
-    if (status != Core::ERROR_NONE) std::this_thread::sleep_for(std::chrono::milliseconds(1000));
     EXPECT_EQ(Core::ERROR_NONE, status);
+    sleep(5);
 }
 
 uint32_t LEDControl_L2test::CreateDeviceLEDControlInterfaceObject()
