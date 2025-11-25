ae657260eaf8598297e024002859505631943010
HarshiniRavichandran <166796069+HarshiniRavichandran@users.noreply.github.com>
2025-11-25 14:08:23 +0530

    RDKEMW-10493 : Update frontpanel.cpp

--- helpers/frontpanel.cpp
+++ helpers/frontpanel.cpp
@@ -454,7 +454,7 @@ namespace WPEFramework
             }
             else if (parameters.HasLabel("red")) //color mode 1
             {
-                unsigned int red = 0,green = 0,blue = 0;
+                unsigned int red = 0, green = 0, blue = 0;
 
                 getNumberParameter("red", red);
                 getNumberParameter("green", green);
@@ -519,7 +519,7 @@ namespace WPEFramework
                 }
                 else if (frontPanelBlinkHash.HasLabel("red")) //color mode 1
                 {
-                    unsigned int red = 0,green = 0,blue = 0;
+                    unsigned int red = 0, green = 0, blue = 0;
 
                     getNumberParameterObject(frontPanelBlinkHash, "red", red);
                     getNumberParameterObject(frontPanelBlinkHash, "green", green);
