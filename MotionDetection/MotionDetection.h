3289d82b52f17509e0e3d228047bf989174af408
HarshiniRavichandran <Harshini_Ravichandran@comcast.com>
2025-12-02 19:13:52 +0530

    Restore file from develop

--- MotionDetection/MotionDetection.h
+++ MotionDetection/MotionDetection.h
@@ -20,7 +20,6 @@
 #pragma once
 
 #include <chrono>
-#include <mutex>
 #include "Module.h"
 #include "motionDetector.h"
 
@@ -84,7 +83,6 @@ namespace WPEFramework {
             //End events
         public:
             static MotionDetection* _instance;
-            static std::mutex _instanceMutex;
 
         private:
             std::chrono::system_clock::time_point m_lastEventTime;
