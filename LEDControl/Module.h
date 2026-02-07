3425df3b499715bb4494ab852125e36b9a8e6a7a
tabbas651 <thamimrazith_abbasali@comcast.com>
2026-02-07 02:01:01 +0000

    RDKEMW-11483: Fix the EntServices-peripherals to support R5.3
    Reason for change: Added entservices-peripherals changes to support the Thunder R5.3
    Test Procedure: please refer the ticket comments
    Risks: Medium
    Signed-off-by: Thamim Razith Abbas Ali tabbas651@cable.comcast.com

--- LEDControl/Module.h
+++ LEDControl/Module.h
@@ -22,8 +22,8 @@
 #define MODULE_NAME Plugin_LEDControl
 #endif
 
+#include <core/core.h>
 #include <plugins/plugins.h>
-#include <tracing/tracing.h>
 
 #undef EXTERNAL
 #define EXTERNAL
--- RemoteControl/Module.h
+++ RemoteControl/Module.h
@@ -22,8 +22,8 @@
 #define MODULE_NAME RemoteControl
 #endif
 
+#include <core/core.h>
 #include <plugins/plugins.h>
-#include <tracing/tracing.h>
 
 #undef EXTERNAL
 #define EXTERNAL
